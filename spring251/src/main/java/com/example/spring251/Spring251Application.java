1b5b95bde209a28ac5a8829fd9a5c7984a0857dd
Ruslan <ruslan.yatsevich@softclub.by>
2021-06-18 16:40:52 +0300

    Two modules for different Spring versions.

--- spring251/src/main/java/com/example/spring251/Spring251Application.java
+++ spring251/src/main/java/com/example/spring251/Spring251Application.java
@@ -1,28 +1,13 @@
 package com.example.spring251;
 
-import com.example.spring251.service.UserService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import javax.transaction.Transactional;
-
 @SpringBootApplication
-public class Spring251Application implements CommandLineRunner {
-
-    @Autowired
-    private UserService userService;
+public class Spring251Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Spring251Application.class, args);
-    }
-
-    @Override
-    @Transactional
-    public void run(String... args) {
-        System.out.println(userService.getUser(1));
-        System.out.println(userService.getUser(2));
     }
 
 }
